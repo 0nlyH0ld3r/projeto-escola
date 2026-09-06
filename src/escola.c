f4b2b0dd827ae5afab184552befb21d4bd404b12
0nlyH0ld3r <manin.d2020@gmail.com>
2026-09-06 20:48:22 -0300

    escola.c: Alterado para que cpf seja tratado como uma string em cadastrar. Adicionado quebras de linhas e reorganizado código em cadastrar.

--- src/escola.c
+++ src/escola.c
@@ -4,8 +4,8 @@
 #include "../headers/utilidades.h"
 
 void cadastrar(void* lista, int index, uint8_t tipo) {
-	if (index / 10000000) {
-		index %= 1000;
+	if (index / ANO_ATUAL) {
+		index %= ANO_ATUAL;
 	}
 	switch (tipo) {
 	case INDIVIDUO:
@@ -20,12 +20,15 @@ void cadastrar(void* lista, int index, uint8_t tipo) {
 }
 
 void input_individuo(individuo* lista, int index) {
-	lista[index].estado	= ATIVO;
+	puts("Digite seu nome: ");
+	input_string(lista[index].nome, 40);
+	puts("\n\n");
 
-	lista[index].matricula	= (2026 * 10000) + index;
+	puts("Digite seu CPF (111.111.111-11): ");
+	input_string(lista[index].cpf, MAX_CHAR_CPF);
+	puts("\n");
 
-	puts("É doscente? (S - Sim | N - Sim)\n");
-
+	puts("É doscente? (S - Sim | N - Sim): ");
 	switch (input_char_non_canon()) {
 	case 'S':
 	case 's':
@@ -37,25 +40,23 @@ void input_individuo(individuo* lista, int index) {
 		lista[index].eh_doscente = false;
 		break;
 	}
-
-	puts("Digite seu nome: ");
-	input_string(lista[index].nome, 40);
-	puts("\n\n");
-
-	puts("Digite seu CPF: ");
-	scanf_limpo_simples("%d", &lista[index].cpf);
 	puts("\n");
 
 	puts("Digite seu gênero (M - masculino | F - feminino) ");
 	lista[index].genero = input_char_non_canon();
+	puts("\n");
 
 	puts("Digite sua data de nascimento (DDMMAAAA): ");
 	int data;
 	scanf_limpo_simples("%d", &data);
+	puts("\n");
+
+	lista[index].estado	= ATIVO;
+	lista[index].matricula	= (2026 * 10000) + index;
+
 	lista[index].nascimento.ano = data % 10000;
 	lista[index].nascimento.mes = data % 1000000 / 10000;
 	lista[index].nascimento.dia = data / 1000000;
-	puts("\n");
 
 	lista[index].n_disciplinas = 0;
 }
