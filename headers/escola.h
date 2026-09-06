feeebe670242dd87d2631859c17041dc192f5445
0nlyH0ld3r <manin.d2020@gmail.com>
2026-09-05 22:31:35 -0300

    escola.h: Adicionado o header <stdint.h>, criado a função input_individuo (WIP para input_disciplina para tornar cadastro mais genérica), adição de novo parâmetro para cadastrar e inicializar_lista (antigo inicializar_individuo)

--- headers/escola.h
+++ headers/escola.h
@@ -1,10 +1,12 @@
 #include "constants.h"
+#include <stdint.h>
 
 
 #ifndef _ESCOLA
 #define _ESCOLA
 
-	void cadastrar(individuo* lista, int index);
-	void inicializar_individuo(individuo* lista, unsigned int tam);
+	void cadastrar(void* lista, int index, uint8_t tipo);
+	void inicializar_lista(void* lista, size_t tam, uint8_t tipo);
+	void input_individuo(individuo* lista, int index);
 
 #endif
