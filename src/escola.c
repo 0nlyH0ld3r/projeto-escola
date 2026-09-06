626110dcadccaca777f0cad6154f35c967cd7428
0nlyH0ld3r <manin.d2020@gmail.com>
2026-09-05 22:36:11 -0300

    escola.c: Criado a função inicializar_lista() para inicializar o parâmetro .estado nos elementos das listas tanto de indivíduos quando de disciplinas. função cadastrar tornada genérica por meio da separação entre os seus inputs para input_individuo e input_disciplina (WIP). A forma como a função lida com a data agora condiz com o novo tipo data { .dia, .mes, .ano}

--- src/escola.c
+++ src/escola.c
@@ -3,45 +3,81 @@
 #include "../headers/escola.h"
 #include "../headers/utilidades.h"
 
-void cadastrar(individuo* lista, int index) {
+void cadastrar(void* lista, int index, uint8_t tipo) {
 	if (index / 10000000) {
 		index %= 1000;
 	}
+	switch (tipo) {
+	case INDIVIDUO:
+		input_individuo((individuo*)lista, index);
+		break;
+	case DISCIPLINA:
+		//input_disciplina((disciplina*)lista, index);
+		break;
 
-	lista[index].estado = ATIVO;
-	lista[index].matricula = (2026 * 10000) + index;
+	}
 
-	puts("É doscente?\n");
+}
+
+void input_individuo(individuo* lista, int index) {
+	lista[index].estado	= ATIVO;
+
+	lista[index].matricula	= (2026 * 10000) + index;
+
+	puts("É doscente? (S - Sim | N - Sim)\n");
 
 	switch (input_char_non_canon()) {
+	case 'S':
 	case 's':
 		lista[index].eh_doscente = true;
+		break;
 
+	case 'N':
 	case 'n':
 		lista[index].eh_doscente = false;
+		break;
 	}
 
 	puts("Digite seu nome: ");
 	input_string(lista[index].nome, 40);
-	puts("\n");
+	puts("\n\n");
 
 	puts("Digite seu CPF: ");
 	scanf_limpo_simples("%d", &lista[index].cpf);
+	puts("\n");
 
 	puts("Digite seu gênero (M - masculino | F - feminino) ");
 	lista[index].genero = input_char_non_canon();
 
-	puts("Digite sua data de nascimento (DDMMAA): ");
-	scanf_limpo_simples("%d", &lista[index].nascimento);
+	puts("Digite sua data de nascimento (DDMMAAAA): ");
+	int data;
+	scanf_limpo_simples("%d", &data);
+	lista[index].nascimento.ano = data % 10000;
+	lista[index].nascimento.mes = data % 1000000 / 10000;
+	lista[index].nascimento.dia = data / 1000000;
 	puts("\n");
 
 	lista[index].n_disciplinas = 0;
 }
 
-void inicializar_individuo(individuo* lista, unsigned int tam) {
-	for (int i = 0; i < tam; ++i) {
-	lista[i].estado = NAO_ATIVO;
+void inicializar_lista(void* lista, size_t tam, uint8_t tipo) {
+	if (tipo == INDIVIDUO) {
+		individuo* ptr_lista = (individuo*)lista;
+
+		for (int i = 0; i < tam; ++i)
+			ptr_lista[i].estado = NAO_ATIVO;
+
+		return;
 	}
 
+	else if (tipo == DISCIPLINA) {
+
+		disciplina* ptr_lista = (disciplina*)lista;
+
+		for (int i = 0; i < tam; ++i)
+			ptr_lista[i].estado = NAO_ATIVO;
+
+		return;
+	}
 }
 
