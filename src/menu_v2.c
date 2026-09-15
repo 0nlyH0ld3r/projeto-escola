c8e765a33e03914865c77bf8e5d3c593053d946f
Yngwazz <ilr.rammos@gmail.com>
2026-09-15 18:51:05 -0300

    Update menuV2

--- src/menu_v2.c
+++ src/menu_v2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <sys/ioctl.h>
 #include "../headers/utilidades.h"
 #include "../headers/relatorio.h"
@@ -9,47 +10,71 @@
 
 #define TAM_LISTA_ALUNOS 5
 #define TAM_LISTA_DISCIPLINAS 3
+#define QNT_LINHAS_MENU 10
+static int contaLinhas = 0;
 
 void menuCadastro(void){
 	printf("MENU CADASTRO");
 }
 
-void menuRelatorio(){
+void menuRelatorio(void){
 	printf("MENU RELATÓRIO");
 }
 
+void printaMenu(struct winsize *w){
+	for(int i =0; i<w->ws_col;i++) printCotabilizado("*");
+    printCotabilizado("\n");
 
+	printCotabilizado("Digite a opção: \n");
+	printCotabilizado("\t0 - Sair \n");
+	printCotabilizado("\t1 - Inserir \n");
+	printCotabilizado("\t2 - Listar \n");
+	printCotabilizado("\t3 - Atualizar \n");
+	printCotabilizado("\t4 - Deletar \n");
+	
+    for(int i =0; i<w->ws_col;i++) printCotabilizado("*");
+    printCotabilizado("\n");
 
+}
+
+void apagaLinhaMenu(int linhasParaApagar){
+	for(int i = 0; i< linhasParaApagar; i++)printf("\033[A\33[2K");
+}
+
+void printCotabilizado(const char *str, ...){
+	for(int i =0; str[i] != '\0'; i++){
+		if(str[i] == '\n') contaLinhas++;
+		fprintf(stdout, str);
+	}
+	//PRINTA COM V_ARGS
+	va_list args;
+	va_start(args, str);
+	vprintf(str, args);
+	va_end(args);
+
+	fflush(stdout);
+}
 void menuv2(escola* escola, int debug){
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-	if(debug) printf("ENTROU MENU.\n\n");
-	system("clear");
-	individuo lista_individuos[TAM_LISTA_ALUNOS];
+	if(debug) printf("ENTROU MENU.\n\n"); else system("clear");
+
+	individuo lista_individuos[TAM_LISTA_ALUNOS]; //fazer isso uma propriedade de escola dps
 	disciplina lista_disciplinas[TAM_LISTA_DISCIPLINAS];
 
-	int sair = 0;
-	int opcao;
-    for(int i =0; i<60;i++) printf("*");
-    printf("\n");
-    printf("Digite a opção: \n\n");
-    printf("\t0 - Sair \n");
-    printf("\t1 - Inserir \n");
-    printf("\t2 - Listar \n");
-    printf("\t3 - Atualizar \n");
-    printf("\t4 - Deletar \n\n");
-    for(int i =0; i<60;i++) printf("*");
-    printf("\n");
+	int opcaoMenu;
+	
 
-    scanf_limpo_simples("%d", &opcao);
-    int total_lines_printed = 10;
-	while(!sair){ //sair == 0
-        if (total_lines_printed >= w.ws_row-1) {
-        printf("\033[A\33[2K");
-        printf("\033[A\33[2K");
-        printf("\033[A\33[2K");
-        
-        total_lines_printed = 10;
+	//PEGA TAMANHO DO TERMINAL
+	struct winsize janela;
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &janela);
+
+	printaMenu(&janela);
+    scanf_limpo_simples("%d", &opcaoMenu);
+
+    /*
+	while(SAIR){ //sair == 0
+        if (linhasMenu >= janela.ws_row) {
+        linhasMenu = 10;
+		apagaLinhaMenu(1);
         }
 		switch (opcao){
 		case SAIR:
@@ -72,7 +97,7 @@ void menuv2(escola* escola, int debug){
 				//listar_disciplinas(lista_disciplinas, TAM_LISTA_DISCIPLINAS);
 				break;
 			}
-			*/
+			
 			menuRelatorio();
             opcao = -1;
 			break;
@@ -87,5 +112,7 @@ void menuv2(escola* escola, int debug){
             total_lines_printed++;
             opcao = -1;
 		}
+
 	}
+	*/
 }
