01877a3619dd1a984e87e33f22c3d2505fc0c4eb
0nlyH0ld3r <manin.d2020@gmail.com>
2026-09-15 17:25:05 -0300

    Nem lembro, esqueci de dar commit antes. Mas tem uma Enum pra jogar em constants, mas evitei mexer

--- headers/escola.h
+++ headers/escola.h
@@ -5,11 +5,15 @@
 #ifndef _ESCOLA
 #define _ESCOLA
 
-	void cadastrar(void* lista, int index, uint8_t tipo);
 	void cadastro_individuo(individuo* lista, size_t tam);
+	void remover_individuo(individuo* pessoa);
+	void atualizar_individuo(individuo* pessoa, int opcao);
+	void input_individuo(individuo* lista, int opcao);
+
 	void cadastro_disciplina(disciplina* lista, individuo* lista_prof, size_t tam);
-	void input_individuo(individuo* lista);
-	void input_disciplina(disciplina* lista, individuo* lista_prof);
+	void remover_disciplina(disciplina* pessoa);
+	void atualizar_disciplina(disciplina* disciplina, int opcao);
+	void input_disciplina(disciplina* lista, individuo* lista_prof, int opcao);
 		
 	#define inicializar_lista(lista, tam)		\
 		for (size_t i = 0; i < tam; ++i){	\
--- src/escola.c
+++ src/escola.c
@@ -4,123 +4,189 @@
 #include "../headers/relatorio.h"
 #include "../headers/utilidades.h"
 
-#if DIVIDIR_EM_DUAS_FUNÇÕES
-void cadastrar(void* lista, int index, uint8_t tipo) {
-	if (index / ANO_ATUAL) index %= ANO_ATUAL;
-
-	if (index == LISTA_CHEIA) {
-		printf("A lista está cheia!\n");
-		return;
-	};
-
-	if (tipo == INDIVIDUO) {
-		individuo* buff_l = (individuo*)lista;
-		input_individuo(&buff_l[index], index);
-	}
-	if (tipo == DISCIPLINA) {
-		// disciplina* buff_l = (disciplina*)lista;
-		// input_disciplina((disciplina*)lista, index);
-	}
-
-}
-#endif
-
+// TODO: Mover isso pra constants.h
+enum opcoes_cadastro_atualizar {
+	GERAL,
+	NOME,
+	CPF,
+	CARGO,
+	GENERO,
+	DATA,
+	CODIGO,
+	PROFESSOR,
+};
 
 void cadastro_individuo(individuo* lista, size_t tam) {
 	size_t index = 0;
 	procura_vaga(lista, tam, index);
-	input_individuo(&lista[index]);
+	input_individuo(&lista[index], GERAL);
 }
+
 void cadastro_disciplina(disciplina* lista, individuo* lista_prof, size_t tam) {
 	size_t index = 0;
 
 	procura_vaga(lista, tam, index);
-	input_disciplina(&lista[index], lista_prof);
-
+	input_disciplina(&lista[index], lista_prof, GERAL);
 }
 
+void remover_individuo(individuo* pessoa) {
+	if (pessoa == NULL) {
+		puts("\n\tPessoa não encontrada!\n");
+		return;
+	}
 
-void input_individuo(individuo* pessoa) {
+	pessoa->estado = NAO_ATIVO;
+}
+
+void remover_disciplina(disciplina* pessoa) {
+	if (pessoa == NULL) {
+		puts("\n\tDisciplina não encontrada!\n");
+		return;
+	}
+
+	pessoa->estado = NAO_ATIVO;
+}
+
+void atualizar_individuo(individuo* pessoa, int opcao) {
+	if (pessoa == NULL) {
+		puts("\n\tPessoa não encontrada!\n");
+		return; 
+	}
+
+	input_individuo(pessoa, opcao);
+}
+
+void atualizar_disciplina(disciplina* disciplina, int opcao) {
+	if (disciplina == NULL) {
+		puts("\n\tDisciplina não encontrada!\n");
+		return; 
+	}
+
+	input_disciplina(disciplina, NULL, opcao);
+}
+
+void input_individuo(individuo* pessoa, int opcao) {
+	if (pessoa == NULL) {
+		puts("\n\tPessoa não encontrada!\n");
+		return;
+	}
+
 	static int primeiros_digitos = 0;
-	puts("Digite seu nome: ");
-	input_string(pessoa->nome, 40);
 
-	puts("\n\nDigite seu CPF (111.111.111-11): ");
-	input_string(pessoa->cpf, MAX_CHAR_CPF);
+	switch (opcao){
+	case GERAL:
+	case NOME:
+		puts("Digite seu nome: ");
+		input_string(pessoa->nome, 40);
 
-	puts("\nÉ doscente? (S - Sim | N - Sim): ");
-	switch (input_char_non_canon()) {
-	case 'S':
-	case 's':
-		pessoa->cargo = DOSCENTE;
-		break;
+		if (opcao != GERAL) return;
 
-	case 'N':
-	case 'n':
-		pessoa->cargo = DISCENTE;
-		break;
+	case CPF:
+		puts("\n\nDigite seu CPF (111.111.111-11): ");
+		input_string(pessoa->cpf, MAX_CHAR_CPF);
+
+		if (opcao != GERAL) return;
+
+	case CARGO:
+		puts("\nÉ doscente? (S - Sim | N - Sim): ");
+		switch (input_char_non_canon()) {
+			case 'S':
+			case 's':
+				pessoa->cargo = DOSCENTE;
+				break;
+
+			case 'N':
+			case 'n':
+				pessoa->cargo = DISCENTE;
+				break;
+		}
+
+		if (opcao != GERAL) return;
+
+	case GENERO:
+		puts("\nDigite seu gênero (M - masculino | F - feminino) ");
+		switch (input_char_non_canon()) {
+			case 'M':
+			case 'm':
+				pessoa->genero = 'M';
+				break;
+
+			case 'F':
+			case 'f':
+				pessoa->genero = 'F';
+				break;
+		}
+
+		if (opcao != GERAL) return;
+
+	case DATA:
+		puts("\nDigite sua data de data (DDMMAAAA): ");
+		int data;
+		scanf_limpo("%d", &data);
+
+		pessoa->data.ano	= data % 10000;
+		pessoa->data.mes	= (data % 1000000) / 10000;
+		pessoa->data.dia	= data / 1000000;
+
+		if (opcao != GERAL) return;
+
 	}
 
-	puts("\nDigite seu gênero (M - masculino | F - feminino) ");
-	switch (input_char_non_canon()) {
-	case 'M':
-	case 'm':
-		pessoa->genero = 'M';
-		break;
-
-	case 'F':
-	case 'f':
-		pessoa->genero = 'F';
-		break;
+	if (pessoa->estado == NAO_ATIVO){
+		pessoa->n_disciplinas	= 0;
+		pessoa->matricula = (2026 * 10000) + primeiros_digitos;
+		printf("\nSua matrícila é: %u\n", pessoa->matricula);
+		++primeiros_digitos;
 	}
 
-	puts("\nDigite sua data de data (DDMMAAAA): ");
-	int data;
-	scanf_limpo("%d", &data);
-	puts("\n");
+	pessoa->estado = ATIVO;
 
-	pessoa->estado			= ATIVO;
-	pessoa->matricula		= (2026 * 10000) + primeiros_digitos;
 
-	pessoa->data.ano		= data % 10000;
-	pessoa->data.mes		= (data % 1000000) / 10000;
-	pessoa->data.dia		= data / 1000000;
-
-	pessoa->n_disciplinas	= 0;
-	++primeiros_digitos;
 }
 
-void input_disciplina(disciplina* disciplina, individuo* lista_prof) {
-	puts("Digite o nome da disciplina: ");
-	input_string(disciplina->nome, MAX_CHAR_NOME);
-	// pessoa->professor;
-	puts("\nDigite o código da disciplina (ex: INF029): ");
-	input_string(disciplina->codigo, MAX_CHAR_COD_DISCIPLINA);
+void input_disciplina(disciplina* disciplina, individuo* lista_prof, int opcao) {
+	if (disciplina == NULL) {
+		puts("\n\tDisciplina não encontrada!\n");
+		return;
+	}
 
-	puts("\nAperte qualquer tecla para entrar no menu de seleção do professor: ");
-	input_char_non_canon();
+	switch (opcao) {
+	case GERAL:
+	case NOME:
+		puts("Digite o nome da disciplina: ");
+		input_string(disciplina->nome, MAX_CHAR_NOME);
+		if (opcao != GERAL) break;
 
-	disciplina->estado			= ATIVO;
-	unsigned int matricula;
+	case CODIGO:
+		puts("\nDigite o código da disciplina (ex: INF029): ");
+		input_string(disciplina->codigo, MAX_CHAR_COD_DISCIPLINA);
+		if (opcao != GERAL) break;
 
-	listar_individuos(lista_prof, MAX_ALUNOS_ESCOLA, NULL, DOSCENTE);
+	case PROFESSOR:
+		puts("\nAperte qualquer tecla para entrar no menu de seleção do professor responsável: ");
+		input_char_non_canon();
 
+		disciplina->estado			= ATIVO;
+		unsigned int matricula;
 
+		listar_individuos(lista_prof, MAX_PESSOAS_ESCOLA, NULL, DOSCENTE);
 
-	do { 
-		puts("\nSelecione o professor pela matrícula: ");
-		scanf_limpo("%u", &matricula);
+		do { 
+			puts("\nSelecione o professor pela matrícula: ");
+			scanf_limpo("%u", &matricula);
 
-		disciplina->professor = busca_matricula(lista_prof, 3, matricula);
+			disciplina->professor = busca_matricula(lista_prof, MAX_PESSOAS_ESCOLA, matricula);
 
-		if (disciplina->professor != NULL) break;
+			if (disciplina->professor != NULL) break;
 
-		puts("\nDigite a matrícula válida de um professor!");
+			puts("\nDigite a matrícula válida de um professor!");
 
-	} while(1);
+		} while(1);
 
-	listar_disciplinas(disciplina, 1, NULL);
-	puts("\n");
+		listar_disciplinas(disciplina, 1, NULL);
+		puts("\n");
+		break;
+	}
 }
 
 
